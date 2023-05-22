cede62fb3ddbff74af461a5512b808efbec92d98
Mary-Fabeyo <132909395+Mary-Fabeyo@users.noreply.github.com>
2023-05-22 13:01:42 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let c;
 
 // alert( "description of alerted_value: " + alerted_value );
 let output = `
-let a = 3; 
+let a = 3;
 let b = 5; 
 let c;\n
 -------------\n
@@ -25,7 +25,6 @@ a > b = ${a > b}
 a < b = ${a < b}
 !a && !c = ${!a && !c}
 !a || !c = ${!a || !c}
-`;
 `;
 
 alert(output);

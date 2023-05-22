6e0e18de7367de01b3203cc66c711569f6a5c4a5
Mary-Fabeyo <132909395+Mary-Fabeyo@users.noreply.github.com>
2023-05-22 13:47:08 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,30 +4,6 @@ let b = 5;
 let c;
 
 // alert( "description of alerted_value: " + alerted_value );
-let output = `
-let a = 3;
-let b = 5; 
-let c;
--------------
-a + b = ${a + b}
-a - b = ${a - b}
-a * b = ${a * b}
-a / b = ${a / b}
-a % b = ${a % b}
-a += b = ${a += b}
-a -= b = ${a -= b}
-a *= b = ${a *= b}
-a /= b = ${a /= b}
-a %= b = ${a %= b}
-a == b = ${a == b}
-a != b = ${a != b}
-a > b = ${a > b}
-a < b = ${a < b}
-!a && !c = ${!a && !c}
-!a || !c = ${!a || !c}
-`;
-
-alert(output);
 /*
 a + b
 a - b
@@ -46,6 +22,16 @@ a < b
 !a && !c 
 !a || !c 
 */
+let output = `
+let a = 3;
+let b = 5; 
+let c;
+-------------
+a + b = ${a + b} \n a - b = ${a - b} \n a * b = ${a * b} \n a / b = ${a / b} \n a % b = ${a % b} \n a += b = ${a += b} \n a -= b = ${a -= b} \n a *= b = ${a *= b} \n a %= b = ${a %= b} \n a == b = ${a == b} \n a != b = ${a != b} \n a > b = ${a > b} \n a < b = ${a < b} \n !a && !c = ${!a && !c} \n !a || !c = ${!a || !c}
+`;
+
+alert(output);
+
 
 
 //TASK 2

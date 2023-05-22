1be944db0f4629b15cc266f9eed32ab247f4c573
Mary-Fabeyo <132909395+Mary-Fabeyo@users.noreply.github.com>
2023-05-22 13:18:35 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let b = 5;
 let c;
 
 // alert( "description of alerted_value: " + alerted_value );
-let output = `let a = 3;\nlet b = 5;\nlet c;\n-------------\na + b = ${a + b}\na - b = ${a - b}\na * b = ${a * b}\na / b = ${a / b}\na % b = ${a % b}\na += b = ${a += b}\na -= b = ${a -= b}\na *= b = ${a *= b}\na /= b = ${a /= b}\na %= b = ${a %= b}\na == b = ${a == b}\na != b = ${a != b}\na > b = ${a > b}\na < b = ${a < b}\n!a && !c = ${!a && !c}\n!a || !c = ${!a || !c}\n`;
+let output = `let a = 3;"\n"let b = 5;"\n"let c;"\n"-------------"\n"a + b = ${a + b}"\n"a - b = ${a - b}"\n"a * b = ${a * b}"\n"a / b = ${a / b}"\n"a % b = ${a % b}"\n"a += b = ${a += b}"\n"a -= b = ${a -= b}"\n"a *= b = ${a *= b}"\n"a /= b = ${a /= b}"\n"a %= b = ${a %= b}"\n"a == b = ${a == b}"\n"a != b = ${a != b}"\n"a > b = ${a > b}"\n"a < b = ${a < b}"\n"!a && !c = ${!a && !c}"\n"!a || !c = ${!a || !c}`;
 
 alert(output);
 /*

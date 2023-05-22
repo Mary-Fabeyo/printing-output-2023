6ddd148a2f1f5025cb1d96d38012d3ca07d137f2
Mary-Fabeyo <132909395+Mary-Fabeyo@users.noreply.github.com>
2023-05-22 13:01:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ let c;
 
 // alert( "description of alerted_value: " + alerted_value );
 let output = `
-let a = 3; \n
-let b = 5; \n
+let a = 3; 
+let b = 5; 
 let c;\n
 -------------\n
-a + b = ${a + b}\n
-a - b = ${a - b}\n
-a * b = ${a * b}\n
-a / b = ${a / b}\n
-a % b = ${a % b}\n
-a += b = ${a += b}\n
-a -= b = ${a -= b}\n
-a *= b = ${a *= b}\n
-a /= b = ${a /= b}\n
-a %= b = ${a %= b}\n
-a == b = ${a == b}\n
-a != b = ${a != b}\n
-a > b = ${a > b}\n
-a < b = ${a < b}\n
-!a && !c = ${!a && !c}\n
-!a || !c = ${!a || !c}\n
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}
+`;
 `;
 
 alert(output);

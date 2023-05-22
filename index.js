2168091216d4b8152515fda39cd1e3471702c53c
Mary-Fabeyo <132909395+Mary-Fabeyo@users.noreply.github.com>
2023-05-22 13:22:05 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,28 @@ let b = 5;
 let c;
 
 // alert( "description of alerted_value: " + alerted_value );
-let output = `let a = 3;"\n"let b = 5;"\n"let c;"\n"-------------"\n"a + b = ${a + b}"\n"a - b = ${a - b}"\n"a * b = ${a * b}"\n"a / b = ${a / b}"\n"a % b = ${a % b}"\n"a += b = ${a += b}"\n"a -= b = ${a -= b}"\n"a *= b = ${a *= b}"\n"a /= b = ${a /= b}"\n"a %= b = ${a %= b}"\n"a == b = ${a == b}"\n"a != b = ${a != b}"\n"a > b = ${a > b}"\n"a < b = ${a < b}"\n"!a && !c = ${!a && !c}"\n"!a || !c = ${!a || !c}`;
+let output = `
+let a = 3;
+let b = 5; 
+let c;
+-------------
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}
+`;
 
 alert(output);
 /*
